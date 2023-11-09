f23f3dea54bea193d4636bc9b03fc15d68d46592
Adem Ahmetovic <ademahmetovic2002@gmail.com>
2023-11-09 20:08:36 +0100

    changed db pass

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ define('BASE_URL', define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/'););
 
 // Database connection parameters.
 define('DB_USER', 'root');
-define('DB_PASS', 'Ad&m2002');
+define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
