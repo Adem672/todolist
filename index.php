612b0ce9588486bcb18416dbb2cec240ef62efa4
Adem Ahmetovic <ademahmetovic2002@gmail.com>
2023-11-09 19:59:35 +0100

    changed host and name of db in index

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 define('BASE_URL', '/');
 
 // Database connection parameters.
-define('DB_USER', 'root');
+define('DB_USER', 'todolist');
 define('DB_PASS', 'Ad&m2002');
 define('DB_NAME', 'todolist');
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
